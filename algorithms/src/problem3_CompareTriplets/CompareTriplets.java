9a159cff5124c624af8f1a84862dc0c35ee9830e
DaniloCerovic87 <chessteacher87@gmail.com>
2024-11-19 23:43:33 +0100

    Added problem CompareTriplets

---
+++ algorithms/src/problem3_CompareTriplets/CompareTriplets.java
@@ -0,0 +1,28 @@
+package problem3_CompareTriplets;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CompareTriplets {
+
+    public static void main(String[] args) {
+        List<Integer> aliceScore = Arrays.asList(1, 2, 3);
+        List<Integer> bobScore = Arrays.asList(3, 2, 1);
+        System.out.println(compareTriplets(aliceScore, bobScore));
+    }
+
+    private static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        int aliceScore = 0;
+        int bobScore = 0;
+
+        for (int i = 0; i < a.size(); i++) {
+            if (a.get(i) > b.get(i)) {
+                aliceScore++;
+            } else if (a.get(i) < b.get(i)) {
+                bobScore++;
+            }
+        }
+
+        return Arrays.asList(aliceScore, bobScore);
+    }
+}
