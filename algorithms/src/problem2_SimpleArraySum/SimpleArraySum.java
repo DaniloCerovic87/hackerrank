d4d070fe1bb49e57f1ffb7e0b68e6b14dfa1ebc4
DaniloCerovic87 <chessteacher87@gmail.com>
2024-11-19 23:35:34 +0100

    Added problem SimpleArraySum

---
+++ algorithms/src/problem2_SimpleArraySum/SimpleArraySum.java
@@ -0,0 +1,22 @@
+package problem2_SimpleArraySum;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SimpleArraySum {
+
+    public static void main(String[] args) {
+        System.out.println(simpleArraySum(Arrays.asList(1, 2, 3, 4, 5)));
+    }
+
+    private static int simpleArraySum(List<Integer> ar) {
+        int sum = 0;
+
+        for (Integer e : ar) {
+            sum += e;
+        }
+
+        return sum;
+    }
+
+}
