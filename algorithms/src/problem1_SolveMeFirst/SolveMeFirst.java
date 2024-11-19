f8185bab47c596030fe6771901ed24d205b2d45b
DaniloCerovic87 <chessteacher87@gmail.com>
2024-11-19 23:33:54 +0100

    Added problem SolveMeFirst

---
+++ algorithms/src/problem1_SolveMeFirst/SolveMeFirst.java
@@ -0,0 +1,15 @@
+package problem1_SolveMeFirst;
+
+public class SolveMeFirst {
+
+    public static void main(String[] args) {
+        System.out.println(solveMeFirst(1, 5));
+    }
+
+    private static int solveMeFirst(int a, int b) {
+        return a + b;
+    }
+
+}
+
+
