e9621edbca87a20dbad235133bf25cc4569fb360
DaniloCerovic87 <chessteacher87@gmail.com>
2025-01-13 12:10:59 +0100

    Added problem Pangrams

---
+++ algorithms/src/problem85_Pangrams/Pangrams.java
@@ -0,0 +1,23 @@
+package problem85_Pangrams;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Pangrams {
+
+    public static void main(String[] args) {
+        System.out.println(pangrams("a_bc"));
+        System.out.println(pangrams("AB    cdefghijklmnOPQ rstuvwxyz"));
+    }
+
+    private static String pangrams(String s) {
+        Set<Character> characters = new HashSet<>();
+        for(char c : s.toLowerCase().toCharArray()) {
+            if(Character.isAlphabetic(c)) {
+                characters.add(c);
+            }
+        }
+        return characters.size() == 26 ? "pangram" : "not pangram";
+    }
+
+}
