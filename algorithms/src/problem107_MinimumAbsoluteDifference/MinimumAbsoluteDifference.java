51b0d7238b2bca2c7caef2590b05dc8ecd7bb645
DaniloCerovic87 <chessteacher87@gmail.com>
2025-01-30 19:01:10 +0100

    Added problem MinimumAbsoluteDifference

---
+++ algorithms/src/problem107_MinimumAbsoluteDifference/MinimumAbsoluteDifference.java
@@ -0,0 +1,27 @@
+package problem107_MinimumAbsoluteDifference;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MinimumAbsoluteDifference {
+
+    public static void main(String[] args) {
+        System.out.println(minimumAbsoluteDifference(Arrays.asList(4, -2, -1)));
+        System.out.println(minimumAbsoluteDifference(Arrays.asList(-59, -36, -13, 1, -53, -92, -2, -96, -54, 75)));
+    }
+
+    public static int minimumAbsoluteDifference(List<Integer> arr) {
+        Collections.sort(arr);
+
+        int minDifference = Integer.MAX_VALUE;
+
+        for (int i = 1; i < arr.size(); i++) {
+            int diff = Math.abs(arr.get(i) - arr.get(i - 1));
+            minDifference = Math.min(minDifference, diff);
+        }
+
+        return minDifference;
+    }
+
+}
