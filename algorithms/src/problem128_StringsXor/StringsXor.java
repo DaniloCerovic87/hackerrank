837f20a0ccd062785d7ff18e4808ca5904ec9cdd
DaniloCerovic87 <chessteacher87@gmail.com>
2025-02-17 13:18:27 +0100

    Added problem StringsXor

---
+++ algorithms/src/problem128_StringsXor/StringsXor.java
@@ -0,0 +1,21 @@
+package problem128_StringsXor;
+
+public class StringsXor {
+
+    public static void main(String[] args) {
+        System.out.println(stringsXOR("10101", "00101"));
+    }
+
+    private static String stringsXOR(String s, String t) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == t.charAt(i))
+                res.append("0");
+            else
+                res.append("1");
+        }
+
+        return res.toString();
+    }
+
+}
