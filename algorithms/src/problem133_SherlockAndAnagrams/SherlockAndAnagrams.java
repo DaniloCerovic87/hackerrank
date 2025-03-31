80e680162aaa0c47eb4748857042bddf9b285afc
Danilo <chessteacher87@gmail.com>
2025-03-31 06:10:53 +0200

    Added one more test case for SherlockAndAnagrams

--- algorithms/src/problem133_SherlockAndAnagrams/SherlockAndAnagrams.java
+++ algorithms/src/problem133_SherlockAndAnagrams/SherlockAndAnagrams.java
@@ -6,6 +6,7 @@ public class SherlockAndAnagrams {
 
     public static void main(String[] args) {
         System.out.println(sherlockAndAnagrams("abba"));
+        System.out.println(sherlockAndAnagrams("kkkk"));
     }
 
     private static int sherlockAndAnagrams(String s) {
